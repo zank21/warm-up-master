6842e1edf60e52f0eea1e148312159c53a373722
zank21 <muradov.sasha@bk.ru>
2021-02-03 18:25:51 +0300

    Проверены все, до этапа оценинивания. Осталось добавить возможность получние таблицы счета. Alpha 1.1

--- game.py
+++ game.py
@@ -1,12 +1,26 @@
-# version Alpha 1.0 (No tests)
+# version Alpha 1.1
+from typing import Union, Any
+import string
+import random
+
+ANSWER_ON_ONE_QUESTION = 2
+
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
+
+
 class User:
     def __init__(self, user_id: int, name: str):
         self.id = user_id
         self.name = name
         self.questions_list = []
-        self.answers_list = []
+        self.question_responded = []
         self.points = 0
         self.last_command = 'None'
+        self.answers_list = []
 
 
 class Question:
@@ -15,7 +29,7 @@ class Question:
         self.text = text
         self.author_id = author_id
         self.round = None
-        self.is_got = False
+        self.users_saw = 0
         self.is_activate = True
 
 
@@ -46,14 +60,21 @@ class Game:
         self.answers_list = []
         self.number_round = 0
         self.round_now = Round(0)
+        self.QUESTIONS_DATABASE: list[Question] = [
+            Question(0, '111111'),
+            Question(0, '222222'),
+            Question(0, '333333'),
+            Question(0, '444444'),
+            Question(0, '555555'),
+        ]
 
     def add_user(self, user: User):
         self.users_list.append(user)
 
     def get_score(self) -> list:
-        score = sorted(self.users_list, key=lambda user: user.points)
-        self.round_now.score = score
-        return score
+        pass
+
+    # Вычитание очков если пользователь не написал вопроса
 
     def new_round(self, round: Round):
         self.rounds_list.append(round)
@@ -65,22 +86,29 @@ class Game:
 
     def add_answer(self, answer: Answer):
         answer.round = self.round_now
+        user_id = answer.author_id
+        user = self.__get_obj_user(user_id)
+        user.answers_list.append(answer)
         self.answers_list.append(answer)
 
     def get_question(self, user_id: int) -> Question:
         user = self.__get_obj_user(user_id)
         for question in self.questions_list:
-            if question.round == self.round_now and question.author_id != user.id and not question.is_got:
-                question.is_got = True
+            if question.round == self.round_now \
+                    and question.author_id != user.id \
+                    and question.users_saw != ANSWER_ON_ONE_QUESTION \
+                    and question not in user.question_responded:
+                user.question_responded.append(question)
+                question.users_saw += 1
                 return question
-            else:
-                return Question(text='<none>', author_id=0)
+        return Question(text='<none>', author_id=0)
 
-    def get_answer_question(self) -> list:
+    def get_answer_question(self) -> Union[tuple[Any, list], list]:
         for question in self.questions_list:
             if question.is_activate and question.round == self.round_now:
                 question.is_activate = False
-                return [question, [answer for answer in self.answers_list if answer.question_id == question.id]]
+                answers = [answer for answer in self.answers_list if answer.question_id == question.id]
+                return question, answers
 
         return []
 
@@ -115,3 +143,17 @@ class Game:
     def add_vote(self, answer_id: int):
         answer = self.__get_obj_answer(answer_id)
         answer.votes += 1
+
+    def __get_question_database(self):
+        for question in self.QUESTIONS_DATABASE:
+            if question.is_activate:
+                question.is_activate = False
+                return question
+        raise Exception('Кончились вопросы в базе данных')
+
+    def check(self):
+        questions = [question for question in self.questions_list if question.round == self.round_now]
+        number_questions_missing = len(self.users_list) * ANSWER_ON_ONE_QUESTION - len(questions)
+        for i in range(number_questions_missing):
+            question = self.__get_question_database()
+            self.questions_list.append(question)
